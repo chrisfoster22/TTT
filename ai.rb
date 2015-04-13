de4d193dd847af620eba21ee4f31a7ef0070791b
Chris Foster <fosterchris22@gmail.com>
2015-04-13 19:09:13 -0400

    Unwinnable.

--- ai.rb
+++ ai.rb
@@ -3,8 +3,7 @@ class Ai
   attr_reader :moves
 
   def initialize
-    @moves = ["M2"]
-    @play = ""
+    @moves = []
   end
 
   def play(board, turn, player)
@@ -13,47 +12,55 @@ class Ai
     possibilities = board.possibilities
     if turn == 1
       puts "I will go first."
-      board.add_play("O", "M", 2)
+      play = "M2"
+    elsif turn == 2
+      if board.edges.include?(last_play) || board.corners.include?(last_play)
+        play = "M2"
+      else
+        play = board.corners.sample
+      end
     elsif turn == 3
       puts "My Turn!"
       if board.edges.include?(last_play)
         if last_play == "T2"
-          @play = ["B1", "B3"].sample
+          play = ["B1", "B3"].sample
         elsif last_play == "B2"
-          @play = ["T1", "T3"].sample
+          play = ["T1", "T3"].sample
         elsif last_play == "M1"
-          @play = ["T3", "B3"].sample
+          play = ["T3", "B3"].sample
         elsif last_play == "M3"
-          @play = ["T1", "B1"].sample
+          play = ["T1", "B1"].sample
         end
       elsif board.corners.include?(last_play)
         if last_play == "T1"
-          @play = "B3"
+          play = "B3"
         elsif last_play == "T3"
-          @play = "B1"
+          play = "B1"
         elsif last_play == "B1"
-          @play = "T3"
+          play = "T3"
         elsif last_play == "B3"
-          @play = "T1"
+          play = "T1"
         end
       end
     elsif turn > 3
-      @play = possibilities.sample
+      play = possibilities.sample
       board.winning_numbers.each do |a|
         if (a & @moves).count == 2
           possible_play = (a - @moves)[0]
-          @play = possible_play if possibilities.include?(possible_play)
-          break
+          if possibilities.include?(possible_play)
+            play = possible_play
+            break
+          end
         elsif (a & player_moves).count == 2
           possible_play = (a - player_moves)[0]
-          @play = possible_play if possibilities.include?(possible_play)
+          play = possible_play if possibilities.include?(possible_play)
         end
       end
     end
-    board.add_play("O", @play[0], @play[1].to_i)
-    possibilities.delete(@play)
+    board.add_play("O", play[0], play[1].to_i)
+    possibilities.delete(play)
     board.display
-    @moves << @play
+    @moves << play
 
   end
 
